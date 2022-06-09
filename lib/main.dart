bce2848fdc4e4820967a26f2154277da9a4f35ce
Hieu <hieugaomoc@gmail.com>
2022-06-09 16:56:21 +0700

    Add Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
-import 'package:flutter/foundation.dart';
+// import 'dart:io';
+
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:device_preview/device_preview.dart';
+import 'package:auth/auth.dart';
+// import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
+// import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '/common/constants.dart';
-import 'common/utils.dart';
+// import 'common/constants.dart';
+// import 'common/utils.dart';
 import 'features/todo/repository.dart';
 import 'features/onboarding/providers.dart';
 import 'features/todo/providers.dart';
@@ -16,6 +20,7 @@ import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   await dotenv.load(fileName: ".env");
   final sharedPreferences = await SharedPreferences.getInstance();
@@ -30,24 +35,36 @@ Future<void> main() async {
   // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
   //     overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
 
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = getSecret(Secrets.sentryDsn);
-      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-      // We recommend adjusting this value in production.
-      options.tracesSampleRate = 1.0;
-    },
-    appRunner: () => runApp(
-      ProviderScope(
-        overrides: [
-          todoRepositoryProvider.overrideWithValue(TodoSharedPrefsRepository(sharedPreferences)),
-          onBoardingSharedPrefsProvider.overrideWithValue(sharedPreferences),
-        ],
-        child: DevicePreview(
-          enabled: !kReleaseMode,
-          builder: (context) => const MyApp(),
-        ),
-      ),
+  // await SentryFlutter.init(
+  //   (options) {
+  //     options.dsn = getSecret(Secrets.sentryDsn);
+  //     // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  //     // We recommend adjusting this value in production.
+  //     options.tracesSampleRate = 1.0;
+  //   },
+  //   appRunner: () => runApp(
+  //     ProviderScope(
+  //       overrides: [
+  //         todoRepositoryProvider.overrideWithValue(TodoSharedPrefsRepository(sharedPreferences)),
+  //         onBoardingSharedPrefsProvider.overrideWithValue(sharedPreferences),
+  //       ],
+  //       child: Platform.isAndroid || Platform.isIOS
+  //           ? const MyApp()
+  //           : DevicePreview(
+  //               enabled: !kReleaseMode,
+  //               builder: (context) => const MyApp(),
+  //             ),
+  //     ),
+  //   ),
+  // );
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        todoRepositoryProvider.overrideWithValue(TodoSharedPrefsRepository(sharedPreferences)),
+        onBoardingSharedPrefsProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: const MyApp(),
     ),
   );
 }
